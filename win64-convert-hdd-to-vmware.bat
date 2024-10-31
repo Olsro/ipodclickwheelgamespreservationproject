f3a53a968d49d819bbc7a5dee1e73685e9743159
Olsro <161404379+Olsro@users.noreply.github.com>
2024-10-31 23:13:26 +0100

    HDD converter windows script is now much more clever to find the qcow2 file and process it

--- win64-convert-hdd-to-vmware.bat
+++ win64-convert-hdd-to-vmware.bat
@@ -1,1 +1,18 @@
-"win64-qemuimg/qemu-img.exe" convert -p -f qcow2 -O vmdk "iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" "win64-vmware/iPod Clickwheel Games Preservation Project.vmdk"
+@echo off
+echo Let's convert the Qemu Virtual HDD (.qcow2) into a VMWare Virtual HDD (.vmdk)
+if exist "iPod.Clickwheel.Games.Preservation.Project.utm/iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" (
+	echo Using path: iPod.Clickwheel.Games.Preservation.Project.utm/iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2
+	echo =================================================================================================
+	echo In progress... don't close this console window until it's finished. This step can take a while...
+	"win64-qemuimg/qemu-img.exe" convert -p -f qcow2 -O vmdk "iPod.Clickwheel.Games.Preservation.Project.utm/iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" "win64-vmware/iPod Clickwheel Games Preservation Project.vmdk"
+) else (
+	if exist "iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" (
+		echo Using path: iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2
+		echo =================================================================================================
+		echo In progress... don't close this console window until it's finished. This step can take a while...
+		"win64-qemuimg/qemu-img.exe" convert -p -f qcow2 -O vmdk "iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" "win64-vmware/iPod Clickwheel Games Preservation Project.vmdk"
+	) else (
+		echo ERROR: Can't find "iPod Clickwheel Games Preservation Project.utm" in your current folder
+		PAUSE
+	)
+)
